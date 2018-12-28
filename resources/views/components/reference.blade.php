8deeddc31c40fab2659e378adf7bba8b1d5861ee
Andrew Earls <andrewearls@rocketmail.com>
2018-12-28 15:30:06 -0800

    references layout

--- resources/views/components/reference.blade.php
+++ resources/views/components/reference.blade.php
@@ -1,6 +1,9 @@
 <div id="Reference" class="row no-gutters justify-content-center">
-	<!-- Reference Image -->
-	<a href="#">
-		<img src="https://via.placeholder.com/150">Reference Name
-	</a>
+	<!-- Start foreach -->
+	<div class="col-3">
+		<a href="#">
+			<img src="https://via.placeholder.com/25">Reference Name
+		</a>
+	</div>
+	<!-- End foreach -->
 </div>
