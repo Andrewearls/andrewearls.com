8150c8233a2795c47ac4c4429d5872a870c923ef
Andrew Earls <andrewearls@rocketmail.com>
2018-12-28 15:43:52 -0800

    downloads layout

--- resources/views/components/download.blade.php
+++ resources/views/components/download.blade.php
@@ -1,3 +1,6 @@
 <div id="Downloads" class="row no-gutters justify-content-center">
-	<a href="#"><img src="https://via.placeholder.com/50">Link</a>
+	<textarea readonly cols="100">
+@include('components.infographic', ['url' => 'http://www.dubiousmacrocosm.com'])
+	</textarea>
+	<!-- <a href="#"><img src="https://via.placeholder.com/50">Link</a> -->
 </div>
