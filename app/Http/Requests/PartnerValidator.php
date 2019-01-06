f1b518cad2b79e29418b93c902687562e46b6ca0
Andrew Earls <andrewearls@rocketmail.com>
2019-01-05 22:53:11 -0800

    partners can be created

--- app/Http/Requests/PartnerValidator.php
+++ app/Http/Requests/PartnerValidator.php
@@ -24,7 +24,7 @@ class PartnerValidator extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required|unique',
             'url' => 'required',
             'image' => 'nullable',
         ];
