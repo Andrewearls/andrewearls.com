0303398675e29bb90669f2b596a2a449cc45493e
Andrew Earls <andrewearls@rocketmail.com>
2019-01-07 11:39:47 -0800

    categories controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Categories;
+use App\Http\Requests\CategoryValidator;
 
 class CategoryController extends Controller
 {
-    //
+    public function create(CategoryValidator $request)
+    {
+    	$validated = $request->validated();
+
+    	//This should be sanitized
+    	$partner = Categories::create($validated);
+        
+    	return 'success';
+    }
+
+    public function update(Request $request)
+    {
+    	# code...
+    }
+
+    public function delete($id)
+    {
+    	# code...
+    }
 }
