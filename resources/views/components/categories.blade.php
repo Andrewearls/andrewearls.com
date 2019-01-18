d601071e9847c46cb3819204d7d736adcb253779
Andrew Earls <andrewearls@rocketmail.com>
2019-01-17 16:04:08 -0800

    category color activation

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,6 +1,6 @@
 <div id="Categories" class="row no-gutters justify-content-center section">
 	@foreach($categories as $category)
-	<div class="col-2 category-object">
+	<div class="col-2 category-object not-active">
 		<a href="">
 			<div class="name">{{ $category->name }}</div>
 		</a>
